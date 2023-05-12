565ca9e05fb6a4f4934d832cfac2e81ae62dbc19
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-05-12 13:46:48 +0300

    Update course controller, add edit

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,5 +1,5 @@
 
-const { createCourse, getById } = require('../services/courseServise');
+const { createCourse, getById, deleteById, editById, updateById } = require('../services/courseServise');
 const { parseError } = require('../util/parser');
 
 const router = require('express').Router();
@@ -33,13 +33,62 @@ router.post('/create', async (req, res) => {
     }
 });
 
+
+
 router.get('/:id', async (req, res) => {
     const course = await getById(req.params.id);
+    const isOwner = course.owner.toString() == req.user._id.toString();
+    const enrolled = true;
 
     res.render('details', {
         title: course.title,
-        body:course
+        body: course,
+        isOwner,
+        enrolled
     });
 });
+
+router.get('/:id/delete', async (req, res) => {
+    const course = await getById(req.params.id);
+
+    if (course && course.owner.toString() != req.user._id) {
+        //console.log('in if');
+        return res.redirect('/auth/login');
+    }
+
+    await deleteById(req.params.id);
+    res.redirect('/');
+});
+
+router.get('/:id/edit', async (req, res) => {
+    const course = await getById(req.params.id);
+    console.log(course._id);
+    res.render('edit', {
+        title: 'Edit course',
+        body: course
+    })
+});
+
+router.post('/:id/edit', async (req, res) => {
+    const course = await getById(req.params.id);
+ console.log('here');
+    if (course.owner.toString() != req.user._id.toString()) {
+        return res.redirect('/auth/login');
+    };
+
+    try {
+       
+        await updateById(req.params.id, req.body);
+        res.redirect(`/course/${req.params.id}`);
+    } catch (err) {
+        console.log('error');
+
+        res.render('edit', {
+            title: 'Edit course',
+            body: req.body,
+            errors: parseError(err)
+        })
+    }
+})
 
 module.exports = router;
